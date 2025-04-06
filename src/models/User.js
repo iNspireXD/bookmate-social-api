39ed4d25df4f4e814c1d72b64a4c3a6d5fbb18f2
Aswin Karki <aswin.karki44@gmail.com>
2025-04-06 17:27:13 +0545

    feat(auth): Implement user registration with validation

--- src/models/User.js
+++ src/models/User.js
@@ -22,6 +22,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+//hash password before saving to database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,73 @@
 import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+
 router.post("/register", async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username must be at least 3 characters" });
+    }
+    if (!email.includes("@")) {
+      return res.status(400).json({ message: "Email is not valid" });
+    }
+
+    // Check if user already exists
+    const existingEmail = await User.findOne({ email: email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUsername = await User.findOne({ username: username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    //get a random profile image from the api
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    // Create new user
+    const user = new User({
+      username,
+      email,
+      password,
+      profileImage,
+    });
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 router.post("/login", async (req, res) => {
