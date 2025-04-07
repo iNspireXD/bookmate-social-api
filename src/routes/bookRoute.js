77ea7a4ff1ef2a25a7ae41dd40df769a60d2bf2c
Aswin Karki <aswin.karki44@gmail.com>
2025-04-07 10:16:53 +0545

    feat(auth): Add auth middleware for route protection

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -1,10 +1,35 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", protectRoute, async (req, res) => {
   try {
-  } catch (error) {}
+    const { title, caption, rating, image } = req.body;
+
+    if (!title || !caption || !rating || !image) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    //upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    //save book to database
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      // user: req.user._id,
+    });
+
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log("Error in creating book:", error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
